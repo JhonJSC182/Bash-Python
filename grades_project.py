0da9e684d2070b2a973597b78d32d5da97744d0d
cyberpunk <jhonjsc2050@gmail.com>
2025-05-31 13:28:36 -0700

    first commit

---
+++ grades_project.py
@@ -0,0 +1,6 @@
+print('Process Student Grades \n')
+
+student_num = int(input('Enter the number of students '))
+
+for i in range(student_num):
+    student_name = input(f"Enter student {i + 1} name: ")
