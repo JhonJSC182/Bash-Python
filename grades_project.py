aa7f62cfff3c5d597b47e4259cf596953e2e14d7
cyberpunk <jhonjsc2050@gmail.com>
2025-06-03 13:15:54 -0700

    finished grading project

--- grades_project.py
+++ grades_project.py
@@ -6,7 +6,7 @@ students = {}
 
 for i in range(student_num):
     student_name = input(f"Enter student {i + 1} name: ")
-    grades = [] / 5
+    grades = []
 
     for j in range(5):
         grade = int(input(f"Enter grade {j + 1} for {student_name} "))
@@ -15,3 +15,27 @@ for i in range(student_num):
 
     students[student_name] = grades
 
+total_average = 0
+
+
+for name, grades in students.items():
+    average = sum(grades) / len(grades)
+    total_average += average
+
+    print(f"{name}'s average grade is {average}")
+    
+    if average >= 90:
+        print(f"{name}'s grade is A")
+    elif average >= 80:
+        print(f"{name}'s grade is B")
+    elif average >= 70:
+        print(f"{name}'s grade is C")
+    elif average >= 60:
+        print(f"{name}'s grade is D")
+    else:
+        print(f"{name}'s grade is F")
+
+    
+class_average = total_average / student_num
+
+print(f"\n The average class grade is {class_average}")
