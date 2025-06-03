77f6829af00caef9683cc6752a1c982ecaf27de7
cyberpunk <jhonjsc2050@gmail.com>
2025-06-02 18:34:56 -0700

    working on python project

--- grades_project.py
+++ grades_project.py
@@ -2,5 +2,16 @@ print('Process Student Grades \n')
 
 student_num = int(input('Enter the number of students '))
 
+students = {}
+
 for i in range(student_num):
     student_name = input(f"Enter student {i + 1} name: ")
+    grades = [] / 5
+
+    for j in range(5):
+        grade = int(input(f"Enter grade {j + 1} for {student_name} "))
+        grades.append(grade)
+
+
+    students[student_name] = grades
+
